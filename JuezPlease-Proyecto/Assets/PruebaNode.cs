5a9dd701f1094cd12919c785e453845e22b6dd04
miguelmplpadilla <89489412+miguelmplpadilla@users.noreply.github.com>
2024-06-21 16:54:28 +0200

    Añadido puguin de nodos para herramienta de creacion de juicios

---
+++ JuezPlease-Proyecto/Assets/PruebaNode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class PruebaNode : Node {
+
+	[Input] public float a;
+	[Output] public float b;
+
+	public GameObject objectPrueba;
+
+	public override object GetValue(NodePort port) {
+		if (port.fieldName == "b") return GetInputValue<float>("a", a);
+		else return null;
+	}
+}
